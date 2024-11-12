29395c29bfb17f41800ac06cd886d0fead01caee
LagaGOD <lupb@itu.dk>
2024-11-12 23:49:09 +0100

    Added a test scene for archer spawning.

---
+++ Prototype/Assets/Scenes/DevScenes/Lucas_Tests/SpawningScene/Config/ExecuteAuthoring.cs
@@ -0,0 +1,22 @@
+using Unity.Entities;
+using UnityEngine;
+
+namespace Scenes.DevScenes.Lucas_Tests.SpawningScene.Config
+{
+    class ExecuteAuthoring : MonoBehaviour
+    {
+        [Header("Spawn Archers (press G to spawn)")] public bool SpawnArchers;
+
+    }
+
+    class ExecuteAuthoringBaker : Baker<ExecuteAuthoring>
+    {
+        public override void Bake(ExecuteAuthoring authoring)
+        {
+            var entity  = GetEntity(TransformUsageFlags.None);
+            if(authoring.SpawnArchers) AddComponent<SpawnArchers>(entity);
+        }
+    }
+    
+    public struct SpawnArchers : IComponentData {}
+}
