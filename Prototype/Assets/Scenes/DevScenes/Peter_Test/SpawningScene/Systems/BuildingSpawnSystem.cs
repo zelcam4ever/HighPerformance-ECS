ad0570b49d7dae1c462f8938f89c60c69c87013b
Peter Klarskov <peter.klarskov@hotmail.com>
2024-12-05 16:26:26 +0100

    wall start

--- Prototype/Assets/Scenes/DevScenes/Peter_Test/SpawningScene/Systems/BuildingSpawnSystem.cs
+++ Prototype/Assets/Scenes/DevScenes/Peter_Test/SpawningScene/Systems/BuildingSpawnSystem.cs
@@ -1,23 +1,42 @@
 using Unity.Burst;
 using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
 
-partial struct BuildingSpawnSystem : ISystem
+namespace Scenes.DevScenes.Peter_Test.SpawningScene
 {
-    [BurstCompile]
-    public void OnCreate(ref SystemState state)
+    partial struct BuildingSpawnSystem : ISystem
     {
-        
-    }
+        [BurstCompile]
+        public void OnCreate(ref SystemState state)
+        {
+            state.RequireForUpdate<Config>();
+            state.RequireForUpdate<SpawnBuildings>();
+        }
 
-    [BurstCompile]
-    public void OnUpdate(ref SystemState state)
-    {
+        [BurstCompile]
+        public void OnUpdate(ref SystemState state)
+        {
+            state.Enabled = false;
+            var config = SystemAPI.GetSingleton<Config>();
+            
+            
+            float3 position = new float3(0, 0, 0);
         
-    }
+            var wallInstance = state.EntityManager.Instantiate(config.WallPrefab);
+            state.EntityManager.SetComponentData(wallInstance, new LocalTransform
+            {
+                Position = position,
+                Rotation = quaternion.identity,
+                Scale = 1.0f
+            });
+            
+        }
 
-    [BurstCompile]
-    public void OnDestroy(ref SystemState state)
-    {
-        
+        [BurstCompile]
+        public void OnDestroy(ref SystemState state)
+        {
+
+        }
     }
 }
