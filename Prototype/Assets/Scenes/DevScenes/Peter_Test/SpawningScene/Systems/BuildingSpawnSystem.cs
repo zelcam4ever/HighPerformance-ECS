845ad26b5832b0c7e8b68b8abf8a06d649c0a3d7
Peter Klarskov <peter.klarskov@hotmail.com>
2024-12-11 12:21:37 +0100

    I tried to author but I dont think it works

--- Prototype/Assets/Scenes/DevScenes/Peter_Test/SpawningScene/Systems/BuildingSpawnSystem.cs
+++ Prototype/Assets/Scenes/DevScenes/Peter_Test/SpawningScene/Systems/BuildingSpawnSystem.cs
@@ -2,6 +2,8 @@ using Unity.Burst;
 using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Transforms;
+using Unity.Physics;
+using Unity.Physics.Authoring;
 
 namespace Scenes.DevScenes.Peter_Test.SpawningScene
 {
@@ -19,10 +21,10 @@ namespace Scenes.DevScenes.Peter_Test.SpawningScene
         {
             state.Enabled = false;
             var config = SystemAPI.GetSingleton<Config>();
-            
-            
-            float3 position = new float3(0, 0, 0);
-        
+
+
+            float3 position = new float3(0, -3, 0);
+
             var wallInstance = state.EntityManager.Instantiate(config.WallPrefab);
             state.EntityManager.SetComponentData(wallInstance, new LocalTransform
             {
@@ -30,13 +32,33 @@ namespace Scenes.DevScenes.Peter_Test.SpawningScene
                 Rotation = quaternion.identity,
                 Scale = 1.0f
             });
-            
+
+            var joints = ComponentLookup<RigidJoint>;
+
         }
+    }
 
-        [BurstCompile]
-        public void OnDestroy(ref SystemState state)
+    [BurstCompile]
+    public struct JointCollisionEvent : IImpulseEventsJob
+    {
+        [ReadOnly] public BufferLookup<PhysicsJointCompanion> JointCompanionBuffer;
+        public EntityCommandBuffer ecb;
+
+        public void Execute(ImpulseEvent impulseEvent)
         {
+            Entity jointToBreak = impulseEvent.JointEntity;
 
+            if (JointCompanionBuffer.HasBuffer(jointToBreak))
+            {
+                var jointCompanionBuffer = JointCompanionBuffer[jointToBreak];
+                for (int i = 0; i < jointCompanionBuffer.Length; i++)
+                {
+                    Debug.Log("DESTROYED" + impulseEvent.Impulse);
+                    ecb.DestroyEntity(jointCompanionBuffer[i].JointEntity);
+                }
+            }
+
+            ecb.DestroyEntity(jointToBreak);
         }
     }
 }
