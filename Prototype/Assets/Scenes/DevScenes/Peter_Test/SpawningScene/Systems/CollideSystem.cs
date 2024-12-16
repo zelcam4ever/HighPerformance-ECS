9468ffe4241e9e9726b6492a0f5640de6a60fb8f
Peter Klarskov <peter.klarskov@hotmail.com>
2024-12-16 17:00:44 +0100

    CollideSystem fuck-up detected and fixed

--- Prototype/Assets/Scenes/DevScenes/Peter_Test/SpawningScene/Systems/CollideSystem.cs
+++ Prototype/Assets/Scenes/DevScenes/Peter_Test/SpawningScene/Systems/CollideSystem.cs
@@ -12,63 +12,69 @@ using UnityEngine.UIElements;
 using BoulderTag = Scenes.DevScenes.Peter_Test.SpawningScene.BoulderTag;
 using SetPhysicsMass = Scenes.DevScenes.Peter_Test.SpawningScene.SetPhysicsMass;
 
-partial struct CollideSystem : ISystem
+namespace Scenes.DevScenes.Peter_Test.SpawningScene
 {
-    
+    partial struct CollideSystem : ISystem
+    {
 
-    [BurstCompile]
-    public void OnUpdate(ref SystemState state)
-    {
-        var triggerJob = new TriggerJob
-        {
-            allBoulders = SystemAPI.GetComponentLookup<BoulderTag>(true),
-            allMasses = SystemAPI.GetComponentLookup<SetPhysicsMass>(),
-            PhysicsMassData = SystemAPI.GetComponentLookup<PhysicsMass>(),
-        }.Schedule(SystemAPI.GetSingleton<SimulationSingleton>(), state.Dependency);
-        triggerJob.Complete();
-        
-    }
 
-    [BurstCompile]
-    public void OnDestroy(ref SystemState state)
-    {
-        
-    }
-}
-[BurstCompile]
-struct TriggerJob : ICollisionEventsJob
-{
-    [ReadOnly] public ComponentLookup<BoulderTag> allBoulders;
-    public  ComponentLookup<SetPhysicsMass> allMasses;
-    public ComponentLookup<PhysicsMass> PhysicsMassData;
-    
-    public void Execute(CollisionEvent collisionEvent)
-    {
-        Entity entityA = collisionEvent.EntityA;
-        Entity entityB = collisionEvent.EntityB;
-        
-        if (allBoulders.HasComponent(entityA) && allMasses.HasComponent(entityB))
+        [BurstCompile]
+        public void OnUpdate(ref SystemState state)
         {
-            var massComponent = allMasses[entityB];
-            var inertiaComponent = PhysicsMassData[entityB];
-            inertiaComponent.InverseInertia = new float3(massComponent.BaselineInertiaX, massComponent.BaselineInertiaY, massComponent.BaselineInertiaZ);
-            PhysicsMassData[entityB] = inertiaComponent;
-            massComponent.Alive = false;
-            massComponent.InfiniteInertiaX = false;
-            massComponent.InfiniteInertiaZ = false;
-            allMasses[entityB] = massComponent;
+            var triggerJob = new TriggerJob
+            {
+                allBoulders = SystemAPI.GetComponentLookup<BoulderTag>(true),
+                allMasses = SystemAPI.GetComponentLookup<SetPhysicsMass>(),
+                PhysicsMassData = SystemAPI.GetComponentLookup<PhysicsMass>(),
+            }.Schedule(SystemAPI.GetSingleton<SimulationSingleton>(), state.Dependency);
+            triggerJob.Complete();
+
         }
-        
-        if (allBoulders.HasComponent(entityB) && allMasses.HasComponent(entityA))
+
+        [BurstCompile]
+        public void OnDestroy(ref SystemState state)
         {
-            var massComponent = allMasses[entityA];
-            var inertiaComponent = PhysicsMassData[entityA];
-            inertiaComponent.InverseInertia = new float3(massComponent.BaselineInertiaX, massComponent.BaselineInertiaY, massComponent.BaselineInertiaZ);
-            PhysicsMassData[entityA] = inertiaComponent;
-            massComponent.Alive = false;
-            massComponent.InfiniteInertiaX = false;
-            massComponent.InfiniteInertiaZ = false;
-            allMasses[entityA] = massComponent;
+
+        }
+    }
+
+    [BurstCompile]
+    struct TriggerJob : ICollisionEventsJob
+    {
+        [ReadOnly] public ComponentLookup<BoulderTag> allBoulders;
+        public ComponentLookup<SetPhysicsMass> allMasses;
+        public ComponentLookup<PhysicsMass> PhysicsMassData;
+
+        public void Execute(CollisionEvent collisionEvent)
+        {
+            Entity entityA = collisionEvent.EntityA;
+            Entity entityB = collisionEvent.EntityB;
+
+            if (allBoulders.HasComponent(entityA) && allMasses.HasComponent(entityB))
+            {
+                var massComponent = allMasses[entityB];
+                var inertiaComponent = PhysicsMassData[entityB];
+                inertiaComponent.InverseInertia = new float3(massComponent.BaselineInertiaX,
+                    massComponent.BaselineInertiaY, massComponent.BaselineInertiaZ);
+                PhysicsMassData[entityB] = inertiaComponent;
+                massComponent.Alive = false;
+                massComponent.InfiniteInertiaX = false;
+                massComponent.InfiniteInertiaZ = false;
+                allMasses[entityB] = massComponent;
+            }
+
+            if (allBoulders.HasComponent(entityB) && allMasses.HasComponent(entityA))
+            {
+                var massComponent = allMasses[entityA];
+                var inertiaComponent = PhysicsMassData[entityA];
+                inertiaComponent.InverseInertia = new float3(massComponent.BaselineInertiaX,
+                    massComponent.BaselineInertiaY, massComponent.BaselineInertiaZ);
+                PhysicsMassData[entityA] = inertiaComponent;
+                massComponent.Alive = false;
+                massComponent.InfiniteInertiaX = false;
+                massComponent.InfiniteInertiaZ = false;
+                allMasses[entityA] = massComponent;
+            }
         }
     }
 }
